9579cfa861ad53736687eadbd75b1375476cbb93
XiaoWeiGe <527188405@qq.com>
2019-05-20 18:02:21 +0800

    LB SUCCESS

--- src/main.js
+++ src/main.js
@@ -4,12 +4,17 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
-import './assets/css/reset.css'
-import './assets/css/border.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'css/reset.css'
+import 'css/border.css'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 /* 通过fastClick解决移动端点击 300毫秒延迟 */
 fastClick.attach(document.body)
+
+// 全局注册swiper插件
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/page/home/components/Swiper.vue
@@ -0,0 +1,16 @@
+<template>
+    
+</template>
+<script>
+export default {
+    name:'HomeSwiper',
+    data(){
+        return {
+            swiperOption:{}
+        }
+    }
+}
+</script>
+<style lang="stylus" scoped>
+    
+</style>
